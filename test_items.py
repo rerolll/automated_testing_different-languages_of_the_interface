0161fd8e1afbf6ad4a40e81080423086aea69ce9
Ilya Kosmakov <kosmakovie@gmail.com>
2024-03-11 14:14:56 +0300

    fix name test and update readme

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_check_exists_button_add_to_basket(browser):
     browser.get(link)
     button_add_to_basket = browser.find_elements(
         By.CSS_SELECTOR, ".btn-add-to-basket")
